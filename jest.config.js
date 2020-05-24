430691e3a66832243172bc0e317bebb67f14c0aa
Andrei Dyatlov <fantazey@gmail.com>
2020-05-24 15:09:34 +0700

    * fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,23 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
   },
-  setupFiles: ['./__tests__/setup.js'],
-  modulePathIgnorePatterns: ["setup.js"],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  coverageReporters: ['text'],
+  setupFiles: ['<rootDir>/__tests__/setup.js'],
+  modulePathIgnorePatterns: [
+    'setup.js',
+    '<rootDit>/public/',
+    '<rootDit>/__tests__/coverage'
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer'
+  ],
+  coverageReporters: [
+    'text'
+  ],
+  coverageDirectory: '<rootDir>/__tests__/coverage/',
   collectCoverageFrom: [
-    "src/**/*.{js,jsx}",
-    "!**/node_modules/**",
-    "!**/vendor/**"
+    'src/**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
   ]
 };
 
